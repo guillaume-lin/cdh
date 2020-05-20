5736c5f33b1147aba613e25122b2dbfbc3fcdfc9
ljx <14100634@qq.com>
2020-05-20 09:28:29 +0800

    mm

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -2,21 +2,33 @@
 import sys
 from kafka import TopicPartition
 from kafka import KafkaConsumer
+from kafka import OffsetAndMetadata
+import json
 
 def main():
-    consumer = KafkaConsumer(bootstrap_servers=["worker2.hengan.shop:9092"],group_id='me',auto_offset_reset = 'earliest',enable_auto_commit=False)
+    consumer = KafkaConsumer(bootstrap_servers=["worker2.hengan.shop:9092"],group_id='me',
+                             auto_offset_reset = 'earliest',
+                             #value_deserializer = lambda m: json.loads(m.decode('utf-8')),
+                             enable_auto_commit=False)
     #consumer.assign([TopicPartition('foobar2',0)])
     consumer.subscribe(['foobar2'])
     #consumer.seek(TopicPartition('foobar2',0),100)
-    #print(consumer.assignment())
+    
     print(consumer.topics())
     print(consumer.subscription())
-    
+    ret = consumer.poll()
+    print(ret)
+    print(consumer.assignment())
+    #consumer.seek_to_beginning()
     try:
         for message in consumer:
             print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,message.offset, message.key,message.value))
             print(consumer.partitions_for_topic('foobar2'))
-            consumer.commit()
+            print("offset is %d" % message.offset)
+            tp1 = TopicPartition(topic="foobar2",partition=0)
+            om = OffsetAndMetadata(offset=message.offset+1,metadata=1)
+            consumer.commit({tp1:om})
+            break
     except KeyboardInterrupt:
         sys.exit()    
 
--- kafka/producer.py
+++ kafka/producer.py
@@ -3,11 +3,12 @@
 
 from kafka import KafkaProducer
 import codecs
+import json
 
 def main():
-    producer = KafkaProducer(bootstrap_servers="worker2.hengan.shop:9092")
-    for i in range(1000):
-        ack = producer.send('foobar2',codecs.encode('my mesage'+str(i)))
+    producer = KafkaProducer(bootstrap_servers="worker2.hengan.shop:9092",value_serializer=lambda m: json.dumps(m).encode('utf-8'))
+    for i in range(1):
+        ack = producer.send('foobar2',{"name":"a"+str(i),"age":i+10})
         metadata = ack.get()
         print(metadata.topic)
         print(metadata.partition)
